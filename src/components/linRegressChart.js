7be139e0ff9e912328079ca03251073ce9e3d367
Danny Paez <dpaez16@yahoo.com>
2019-12-26 17:01:45 -0600

    fixed bug with x-tick labels not displaying decimals

--- src/components/linRegressChart.js
+++ src/components/linRegressChart.js
@@ -102,16 +102,57 @@ const getOptions = (points, labels) => {
     };
 };
 
-const range = (low, high) => { 
-    return Array.from(new Array(high - low + 1), (_, i) => i + low);
+const pointsCompare = (a, b) => {
+    if (a.x > b.x)
+        return 1;
+    else if (a.x < b.x)
+        return -1;
+    else
+        return 0;
 };
+
+const merge = (labels, otherLabels) => {
+    let newLabels = [];
+    let i = 0, j = 0;
+    while (i < labels.length || j < otherLabels.length) {
+        if (i < labels.length && j < otherLabels.length) {
+            if (labels[i] < otherLabels[j].x) {
+                newLabels.push(labels[i]);
+                i++;
+            } else {
+                newLabels.push(otherLabels[j].x);
+                j++;
+            }
+        } else if (i < labels.length) {
+            newLabels.push(labels[i]);
+            i++;
+        } else {
+            newLabels.push(otherLabels[j].x);
+            j++;
+        }
+    }
+
+    return newLabels;
+};
+
+const range = (low, high, points) => { 
+    let labels = Array.from(new Array(high - low + 1), (_, i) => i + low);
+    let otherLabels = points.filter((point) => {
+        return !Number.isInteger(point.x);
+    });
+    otherLabels.sort(pointsCompare);
+
+    let allLabels = merge(labels, otherLabels);
+    return allLabels;
+};
+
 
 export class LinRegressChart extends Component {
     constructor(props) {
         super(props);
         this.state = {
             points: this.props.points,
-            labels: range(this.props.metadata.pts[0].x, this.props.metadata.pts[1].x),
+            labels: range(this.props.metadata.pts[0].x, this.props.metadata.pts[1].x, this.props.points),
             metadata: this.props.metadata,
             toggle: this.props.toggle
         };
@@ -121,7 +162,7 @@ export class LinRegressChart extends Component {
         if (this.props.toggle !== prevProps.toggle) {
             this.setState({
                 points: this.props.points,
-                labels: range(this.props.metadata.pts[0].x, this.props.metadata.pts[1].x),
+                labels: range(this.props.metadata.pts[0].x, this.props.metadata.pts[1].x, this.props.points),
                 metadata: this.props.metadata,
                 toggle: this.props.toggle
             });
