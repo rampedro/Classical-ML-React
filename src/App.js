804e0e653fbda3cae7ccbc082569ac805c4e2a5c
Danny Paez <dpaez16@yahoo.com>
2019-12-26 18:05:10 -0600

    fixed component file structure

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Points} from './components/points'
-import {AddPointForm} from './components/addPointForm';
-import {LinRegressChart} from './components/linRegressChart';
+import {LinRegress} from './components/lin-regress/linRegress';
 import { Container } from 'semantic-ui-react';
 
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            points: [{x: 1, y: 2}, {x: 2, y: 1}, {x: 3, y: 4}],
-            metadata: {
-                pts: [{x: 1, y: 1.33}, {x: 3, y: 3.33}],
-                m: 1,
-                b: 0.33,
-                residual: 0
-            },
-            toggle: 0
-        };
-    };
-
     render() {
         return (
             <Container>
-                <AddPointForm 
-                    points={this.state.points}
-                    onNewPoint={
-                        point => this.setState({
-                            points: [...this.state.points, point]
-                        })
-                    }
-                    updateMetadata={
-                        newMetadata => this.setState({
-                            metadata: newMetadata,
-                            toggle: (this.state.toggle + 1) % 2
-                        })
-                    }
-                />
-                <Points 
-                    points={this.state.points}
-                    toggle={this.state.toggle}
-                    deletePoint={
-                        i => this.setState({
-                                points: this.state.points.filter((_, idx) => i !== idx),
-                                toggle: (this.state.toggle + 1) % 2
-                            })
-                    }
-                />
-                <LinRegressChart
-                    points={this.state.points}
-                    metadata={this.state.metadata}
-                    toggle={this.state.toggle}
-                />
+                <LinRegress />
             </Container>
         );
     }
---
+++ src/components/lin-regress/linRegress.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {Points} from './points'
+import {AddPointForm} from './addPointForm';
+import {LinRegressChart} from './linRegressChart';
+import { Container } from 'semantic-ui-react';
+
+
+export class LinRegress extends Component {
+    constructor() {
+        super();
+        this.state = {
+            points: [{x: 1, y: 2}, {x: 2, y: 1}, {x: 3, y: 4}],
+            metadata: {
+                pts: [{x: 1, y: 1.33}, {x: 3, y: 3.33}],
+                m: 1,
+                b: 0.33,
+                residual: 0
+            },
+            toggle: 0
+        };
+    };
+
+    render() {
+        return (
+            <Container>
+                <AddPointForm 
+                    points={this.state.points}
+                    onNewPoint={
+                        point => this.setState({
+                            points: [...this.state.points, point]
+                        })
+                    }
+                    updateMetadata={
+                        newMetadata => this.setState({
+                            metadata: newMetadata,
+                            toggle: (this.state.toggle + 1) % 2
+                        })
+                    }
+                />
+                <Points 
+                    points={this.state.points}
+                    toggle={this.state.toggle}
+                    deletePoint={
+                        i => this.setState({
+                                points: this.state.points.filter((_, idx) => i !== idx),
+                                toggle: (this.state.toggle + 1) % 2
+                            })
+                    }
+                />
+                <LinRegressChart
+                    points={this.state.points}
+                    metadata={this.state.metadata}
+                    toggle={this.state.toggle}
+                />
+            </Container>
+        );
+    }
+};
