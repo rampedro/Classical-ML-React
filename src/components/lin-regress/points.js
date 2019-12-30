3625639ea73a31d5598b79cc36c60ce998481745
Danny Paez <dpaez16@yahoo.com>
2019-12-30 13:25:34 -0600

    scroll bar for points added

--- src/components/lin-regress/points.js
+++ src/components/lin-regress/points.js
@@ -22,26 +22,28 @@ export class Points extends Component {
         return (
             <div className="lin-regress__points">
                 <h2><u>Points</u>:</h2>
-                <List>
-                {this.state.points.map((point, i) => {
-                    return (
-                        <List.Item key={i}>
-                            <header className='point-row'>
-                                <span className='point-row__point'>
-                                    ({point.x}, {point.y})
-                                </span>
-                                <Button className='point-row__delete'
-                                    onClick={_ => {
-                                        this.state.deletePoint(i);
-                                    }
-                                }>
-                                    <Icon name='close' />
-                                </Button>
-                            </header>
-                        </List.Item>
-                    );
-                })}
-                </List>
+                <div className="lin-regress__points-list">
+                    <List>
+                    {this.state.points.map((point, i) => {
+                        return (
+                            <List.Item key={i}>
+                                <header className='point-row'>
+                                    <span className='point-row__point'>
+                                        ({point.x}, {point.y})
+                                    </span>
+                                    <Button className='point-row__delete'
+                                        onClick={_ => {
+                                            this.state.deletePoint(i);
+                                        }
+                                    }>
+                                        <Icon name='close' />
+                                    </Button>
+                                </header>
+                            </List.Item>
+                        );
+                    })}
+                    </List>
+                </div>
             </div>
         );
     }
