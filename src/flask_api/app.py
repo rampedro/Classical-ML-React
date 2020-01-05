c36a17baf541adeccc7a08115c1f97fcdd510094
Danny Paez <dpaez16@yahoo.com>
2020-01-05 11:56:08 -0600

    moved api files to folder, refactored app.py

--- src/flask_api/app.py
+++ src/flask_api/app.py
@@ -1,27 +1,27 @@
-from lin_regress import linRegression
-from svm import svm
-from kmeans import kmeans
+from modules.lin_regress import linRegression
+from modules.svm import svm
+from modules.kmeans import kmeans
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+services = {
+    'svm': svm,
+    'lin_regress': linRegression,
+    'kmeans': kmeans
+}
 
-@app.route('/lin_regress', methods=['PUT'])
-def linearRegression():
+@app.route('/<string:service_name>', methods=['PUT'])
+def service(service_name):
+    try:
+        service_func = services[service_name]
+    except:
+        # service does not exist
+        return None, 401
+    
     data = request.get_json()
-    output_data = linRegression(data)
+    output_data = service_func(data)
     return jsonify(output_data)
 
-@app.route('/svm', methods=['PUT'])
-def SVM():
-    data = request.get_json()
-    output_data = svm(data)
-    return jsonify(output_data)
-
-@app.route('/kmeans', methods=['PUT'])
-def KMeans():
-    data = request.get_json()
-    output_data = kmeans(data)
-    return jsonify(output_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/flask_api/modules/kmeans.py
+++ src/flask_api/modules/kmeans.py
@@ -1,28 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
 
-def makeLine(xx, yy):
-    leftPoint = {'x': xx[0], 'y': np.round(yy[0], 4)}
-    rightPoint = {'x': xx[1], 'y': np.round(yy[1], 4)}
-    return leftPoint, rightPoint
-
-
-def getColors(clf, xx, yy_up, yy_down):
-    colors = ['#000000']
-    predicted = clf.predict([[xx[0], yy_up[0]]])
-    if np.max(predicted) == 1:
-        colors.append('#FF0000')
-    else:
-        colors.append('#0000FF')
-
-    predicted = clf.predict([[xx[0], yy_down[0]]])
-    if np.max(predicted) == 1:
-        colors.append('#FF0000')
-    else:
-        colors.append('#0000FF')
-    
-    return colors
-
 
 def kmeans(data):
     x = np.array(data["x"], dtype='float')
@@ -44,7 +22,5 @@ def kmeans(data):
         'centroids': [{'x': centroids[i, 0], 'y': centroids[i, 1], 'label': i} for i in range(len(centroids))],
         'points': [{'x': D[i, 0], 'y': D[i, 1], 'label': int(labels[i])} for i in range(len(labels))]
     }
-
-    print(output_data)
 
     return output_data
