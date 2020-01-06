13fa614b75d11bebcd82db7f500ac784a5dc77e9
Danny Paez <dpaez16@yahoo.com>
2020-01-05 22:40:29 -0600

    basic navbar works, needs css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import {LinRegress} from './components/lin-regress/linRegress';
 import {SVM} from './components/svm/svm';
 import {KMeans} from './components/kmeans/kmeans';
 import {KMedoids} from './components/kmedoids/kmedoids';
+import {NavBar} from './components/navbar/navbar';
 import { Container } from 'semantic-ui-react';
 import './App.css';
 
@@ -10,9 +12,18 @@ import './App.css';
 export default class App extends Component {
     render() {
         return (
-            <Container>
-                <KMedoids />
-            </Container>
+            <BrowserRouter>
+                <Container>
+                    <NavBar />
+                    <Switch>
+                        <Redirect from='/' to='/linear-regression' exact />
+                        <Route path='/linear-regression' component={LinRegress} />
+                        <Route path='/svm' component={SVM} />
+                        <Route path='/k-means' component={KMeans} />
+                        <Route path='/k-medoids' component={KMedoids} />
+                    </Switch>
+                </Container>
+            </BrowserRouter>
         );
     }
 };
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './navbar.css';
+
+export const NavBar = props => {
+    return (
+        <header className='navbar'>
+            <div className='navbar__icon'>
+                <h1>
+                    Classical ML React
+                </h1>
+            </div>
+            <nav className='navbar__item'>
+                <ul>
+                    <li>
+                        <NavLink to='/linear-regression'>Linear Regression</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/svm'>SVM</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/k-means'>K-Means</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/k-medoids'>K-Medoids</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
