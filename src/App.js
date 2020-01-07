a336300f89c5832bac1dcaf1b3b5f5a53ef50ae6
Danny Paez <dpaez16@yahoo.com>
2020-01-06 18:47:38 -0600

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {LinRegress} from './components/lin-regress/linRegress';
 import {SVM} from './components/svm/svm';
 import {KMeans} from './components/kmeans/kmeans';
 import {KMedoids} from './components/kmedoids/kmedoids';
 import {NavBar} from './components/navbar/navbar';
-import { Container } from 'semantic-ui-react';
+import {HomePage} from './components/home/homePage';
 import './App.css';
 
 
@@ -17,11 +17,11 @@ export default class App extends Component {
                     <NavBar />
                     <div className='main-content'>
                         <Switch>
-                            <Redirect from='/' to='/linear-regression' exact />
                             <Route path='/linear-regression' component={LinRegress} />
                             <Route path='/svm' component={SVM} />
                             <Route path='/k-means' component={KMeans} />
                             <Route path='/k-medoids' component={KMedoids} />
+                            <Route path='/' component={HomePage} />
                         </Switch>
                     </div>
                 </React.Fragment>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import './navbar.css';
 
-export const NavBar = props => {
+export const NavBar = _ => {
     return (
         <header className='navbar'>
             <div className='navbar__icon'>
