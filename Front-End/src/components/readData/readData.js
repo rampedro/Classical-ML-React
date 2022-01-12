42ea9c1c7a46a95c20c5870dea5cd21a37d79758
rampedro <pedahd@gmail.com>
2022-01-12 13:54:05 -0500

    Represents the data from back-end

--- Front-End/src/components/readData/readData.js
+++ Front-End/src/components/readData/readData.js
@@ -3,30 +3,48 @@ import { Header } from 'semantic-ui-react';
 
 
 export class ReadData extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            stateData : 'Empty'
+        }
     };
 
-    componentDidMount() {
-        this.fetchApiToEntries('http://127.0.0.1:5000/readdata');
-      }
-      fetchApiToEntries = (apiToFetch) => {
-          fetch(apiToFetch)
-              .then(result => {
-              result.json().then(data => {
-                console.log(data);
 
-              })
-              .catch((error) => console.log(error.message));
-            })};
+
+    getData = (apiToFetch) => {
+        fetch(apiToFetch)
+            .then(result => {
+            result.json().then(data => {
+              //console.log(data);
+              // Settign the state ( making the changes to be render soon after)
+              this.setState({stateData : data})
+
+            })
+            .catch((error) => console.log(error.message));
+          })};
+
+
+
+    componentDidMount() {
+        this.getData('http://127.0.0.1:5000/readdata');
+      }
+
+
+
     render() {
         return (
             <div>
                 <Header className='title'
                         size='huge'>
-                    This is my test page to read data from python program
+
+                 Loading data from Python backend !
                 </Header>
                 <div className="tester">
+                <div>  
+                    {this.state.stateData}
+                </div>
+                
                    
                 </div>        
             </div>
