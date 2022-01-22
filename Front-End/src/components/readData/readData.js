a85cbeca2965b5fd8e750f2b95d99f3df986fb28
rampedro <pedahd@gmail.com>
2022-01-22 17:52:39 -0500

    Adds more examples to ReadData page

--- Front-End/src/components/readData/readData.js
+++ Front-End/src/components/readData/readData.js
@@ -1,34 +1,63 @@
-import React, {Component,useEffect} from 'react';
+import React, { Component, useEffect } from 'react';
 import { Header } from 'semantic-ui-react';
+import { Points } from './points';
+import { LinRegressChart } from './linRegressChart';
+import {AddPersonFrom} from './AddPersonFrom'
+import { List, Button, Icon } from 'semantic-ui-react';
+import './points.css';
+
 
 
 export class ReadData extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            stateData : 'Empty'
+            stateData: 'Empty',
+            points: [{ x: 1, y: 2 }, { x: 2, y: 1 }, { x: 3, y: 4 }],
+            metadata: {
+                bestFitLine: [{ x: 1, y: 1.33 }, { x: 3, y: 3.33 }],
+                m: 1,
+                b: 0.33,
+                residual: 2.67
+            },
+            toggle: 0
+
         }
     };
 
 
+    // Defining a fnction getData to later be useed as getData(url)
 
     getData = (apiToFetch) => {
         fetch(apiToFetch)
             .then(result => {
-            result.json().then(data => {
-              //console.log(data);
-              // Settign the state ( making the changes to be render soon after)
-              this.setState({stateData : data})
+                result.json().then(data => {
+                    //console.log(data);
+                    // Settign the state ( making the changes to be render soon after)
+                    this.setState({ stateData: data })
 
+                })
+                    .catch((error) => console.log(error.message));
             })
-            .catch((error) => console.log(error.message));
-          })};
+    };
 
 
+    // Mounting component lets calls the getdata(url)
 
     componentDidMount() {
         this.getData('http://127.0.0.1:5000/readdata');
-      }
+    }
+
+
+
+
+    // Creating a contact manager form
+
+
+
+
+
+
 
 
 
@@ -36,17 +65,47 @@ export class ReadData extends Component {
         return (
             <div>
                 <Header className='title'
-                        size='huge'>
+                    size='huge'>
 
-                 Loading data from Python backend !
+                    Loading data from Python backend !
                 </Header>
-                <div className="tester">
-                <div>  
-                    {this.state.stateData}
-                </div>
-                
-                   
-                </div>        
+
+                <AddPersonFrom />
+
+
+                <g>
+                    <div className="tester">
+                        <div className="lin-regress__points-list">
+                            <p>
+                                This is just a json example returned from Python BackEnd.
+                                {this.state.stateData}
+                            </p>
+
+
+                        </div>
+
+
+                        <Points
+                            points={this.state.points}
+                            toggle={this.state.toggle}
+                            deletePoint={
+                                i => this.setState({
+                                    points: this.state.points.filter((_, idx) => i !== idx),
+                                    toggle: (this.state.toggle + 1) % 2
+                                })
+                            }
+                        />
+
+
+
+                        <LinRegressChart
+                            points={this.state.points}
+                            bestFitLine={this.state.metadata.bestFitLine}
+                        />
+
+
+                    </div>
+                </g>
             </div>
         );
     }
