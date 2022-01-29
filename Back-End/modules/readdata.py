b8f3b49c43475b5c9f361b59201b6cfc9e61a381
rampedro <pedahd@gmail.com>
2022-01-29 18:46:36 -0500

    Sets up a Docker Compose

--- Back-End/modules/readdata.py
+++ Back-End/modules/readdata.py
@@ -2,7 +2,7 @@ import sys
 
 def readdata(a):
     import pandas as pd
-    path = "covid-variants.csv"
+    path = "/data/covid-variants.csv"
     df = pd.read_csv(path)
     output_data = df.groupby(by='location')['num_sequences_total'].sum().sort_values(ascending=False).head()
     output_data = output_data.to_json()
