3adbe346456cc359b95780557e19265336095bd7
rampedro <pedahd@gmail.com>
2022-01-29 17:54:45 -0500

    adds deployment parameters

--- Back-End/modules/readdata.py
+++ Back-End/modules/readdata.py
@@ -1,6 +1,9 @@
+import sys
+
 def readdata(a):
     import pandas as pd
-    df = pd.read_csv('/Users/book/Desktop/javascript/remlf3/Classical-ML-React/Back-End/data/covid-variants.csv')
+    path = "covid-variants.csv"
+    df = pd.read_csv(path)
     output_data = df.groupby(by='location')['num_sequences_total'].sum().sort_values(ascending=False).head()
     output_data = output_data.to_json()
     
